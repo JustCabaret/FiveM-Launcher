ed1dd3f2a68780cbe9a8ca7398cec952d641f826
JustCabaret <hugocabaretdasilva@gmail.com>
2022-05-31 17:14:38 +0100

    Retirar número de players funcional.

--- FiveM Launcher/FiveM Launcher/Main.cs
+++ FiveM Launcher/FiveM Launcher/Main.cs
@@ -27,35 +27,22 @@ namespace FiveM_Launcher
 
         private void Main_Load(object sender, EventArgs e)
         {
-            //var result = "";
-            //var uri = "https://servers-frontend.fivem.net/api/servers/single/kx5la6";
-            //var httpRequest = (HttpWebRequest)WebRequest.Create(uri);
-            //httpRequest.Method = "GET";
-            //httpRequest.Headers.Add("User-Agent:", "Mozilla/5.0");
-            //httpRequest.Accept = "application/json";
-            //httpRequest.ContentType = "application/json";
-
-            //var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-
-            //using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            //{
-            //    result = streamReader.ReadToEnd();
-            //}
-
-            ////DataTable dt = JsonConvert.DeserializeObject<DataTable>(result);
-
-            //MessageBox.Show(result);
-
+            var result = "";
             var url = "https://servers-frontend.fivem.net/api/servers/single/kx5la6";
             var httpRequest = (HttpWebRequest)WebRequest.Create(url);
             httpRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36";
+            httpRequest.Accept = "application/json";
+
             var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
-                var result = streamReader.ReadToEnd();
+                result = streamReader.ReadToEnd();
             }
 
-            Console.WriteLine(httpResponse.StatusCode);
+            Servidor servidor = JsonConvert.DeserializeObject<Servidor>(result);
+
+            Console.WriteLine(servidor.Data.clients);
         }
     }
 }
--- FiveM Launcher/FiveM Launcher/Servidor.cs
+++ FiveM Launcher/FiveM Launcher/Servidor.cs
@@ -6,14 +6,68 @@ using System.Threading.Tasks;
 
 namespace FiveM_Launcher
 {
-    internal class Servidor
+    public class Servidor
     {
-        public int nome { get; set; }
+        public string EndPoint { get; set; }
+        public Data Data { get; set; }
+    }
 
-        public int nplayers { get; set; }
+    public class Data
+    {
+        public int clients { get; set; }
+        public string gametype { get; set; }
+        public string hostname { get; set; }
+        public string mapname { get; set; }
+        public int sv_maxclients { get; set; }
+        public bool enhancedHostSupport { get; set; }
+        public string[] resources { get; set; }
+        public string server { get; set; }
+        public Vars vars { get; set; }
+        public int selfReportedClients { get; set; }
+        public Player[] players { get; set; }
+        public int ownerID { get; set; }
+        public bool _private { get; set; }
+        public bool fallback { get; set; }
+        public string[] connectEndPoints { get; set; }
+        public int upvotePower { get; set; }
+        public int burstPower { get; set; }
+        public string support_status { get; set; }
+        public int svMaxclients { get; set; }
+        public string ownerName { get; set; }
+        public string ownerProfile { get; set; }
+        public string ownerAvatar { get; set; }
+        public DateTime lastSeen { get; set; }
+        public int iconVersion { get; set; }
+    }
 
-        public int maxplayers { get; set; }
+    public class Vars
+    {
+        public string Developer { get; set; }
+        public string Discord { get; set; }
+        public string ServerOwner { get; set; }
+        public string banner_connecting { get; set; }
+        public string banner_detail { get; set; }
+        public string gamename { get; set; }
+        public string locale { get; set; }
+        public string onesync_enabled { get; set; }
+        public string sv_enforceGameBuild { get; set; }
+        public string sv_enhancedHostSupport { get; set; }
+        public string sv_lan { get; set; }
+        public string sv_licenseKeyToken { get; set; }
+        public string sv_maxClients { get; set; }
+        public string sv_projectDesc { get; set; }
+        public string sv_projectName { get; set; }
+        public string sv_scriptHookAllowed { get; set; }
+        public string premium { get; set; }
+        public string tags { get; set; }
+    }
 
-        public int status { get; set; }
+    public class Player
+    {
+        public string endpoint { get; set; }
+        public int id { get; set; }
+        public string[] identifiers { get; set; }
+        public string name { get; set; }
+        public int ping { get; set; }
     }
 }
