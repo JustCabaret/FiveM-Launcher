e2dd706badab1d0c49eb7d652dff0d851079cdd8
JustCabaret <hugocabaretdasilva@gmail.com>
2022-06-01 20:30:31 +0100

    Adição do estado do servidor e de número de jogadores.

--- FiveM Launcher/FiveM Launcher/Main.Designer.cs
+++ FiveM Launcher/FiveM Launcher/Main.Designer.cs
@@ -31,14 +31,18 @@ namespace FiveM_Launcher
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btnEntrar = new System.Windows.Forms.Button();
-            this.nplayers = new System.Windows.Forms.Label();
-            this.totalplayers = new System.Windows.Forms.Label();
             this.btnDiscordServer = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnEntrar
@@ -46,32 +50,16 @@ namespace FiveM_Launcher
             this.btnEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(69)))), ((int)(((byte)(121)))));
             this.btnEntrar.FlatAppearance.BorderSize = 0;
             this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEntrar.ForeColor = System.Drawing.Color.White;
-            this.btnEntrar.Location = new System.Drawing.Point(494, 164);
+            this.btnEntrar.Location = new System.Drawing.Point(660, 146);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(4);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(219, 88);
+            this.btnEntrar.Size = new System.Drawing.Size(292, 108);
             this.btnEntrar.TabIndex = 0;
             this.btnEntrar.Text = "Entrar no Servidor";
             this.btnEntrar.UseVisualStyleBackColor = false;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
-            // 
-            // nplayers
-            // 
-            this.nplayers.AutoSize = true;
-            this.nplayers.Location = new System.Drawing.Point(541, 47);
-            this.nplayers.Name = "nplayers";
-            this.nplayers.Size = new System.Drawing.Size(35, 13);
-            this.nplayers.TabIndex = 1;
-            this.nplayers.Text = "label1";
-            // 
-            // totalplayers
-            // 
-            this.totalplayers.AutoSize = true;
-            this.totalplayers.Location = new System.Drawing.Point(606, 46);
-            this.totalplayers.Name = "totalplayers";
-            this.totalplayers.Size = new System.Drawing.Size(35, 13);
-            this.totalplayers.TabIndex = 2;
-            this.totalplayers.Text = "label1";
             // 
             // btnDiscordServer
             // 
@@ -79,78 +67,161 @@ namespace FiveM_Launcher
             this.btnDiscordServer.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(69)))), ((int)(((byte)(121)))));
             this.btnDiscordServer.FlatAppearance.BorderSize = 0;
             this.btnDiscordServer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDiscordServer.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold);
             this.btnDiscordServer.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.btnDiscordServer.Location = new System.Drawing.Point(494, 283);
+            this.btnDiscordServer.Location = new System.Drawing.Point(660, 289);
+            this.btnDiscordServer.Margin = new System.Windows.Forms.Padding(4);
             this.btnDiscordServer.Name = "btnDiscordServer";
-            this.btnDiscordServer.Size = new System.Drawing.Size(219, 88);
+            this.btnDiscordServer.Size = new System.Drawing.Size(292, 108);
             this.btnDiscordServer.TabIndex = 3;
-            this.btnDiscordServer.Text = "Abrir o Discord";
+            this.btnDiscordServer.Text = "Discord do Servidor";
             this.btnDiscordServer.UseVisualStyleBackColor = false;
             this.btnDiscordServer.Click += new System.EventHandler(this.btnDiscordServer_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnDiscordServer);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnEntrar);
+            this.panel1.Location = new System.Drawing.Point(29, 41);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1091, 517);
+            this.panel1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(707, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Offline";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(857, 433);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Jogadores :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(630, 433);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Estado :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bernard MT Condensed", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(51)))), ((int)(((byte)(45)))));
+            this.label1.Location = new System.Drawing.Point(622, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 72);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Junta-te a nós!";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(69)))), ((int)(((byte)(121)))));
+            this.panel2.Location = new System.Drawing.Point(519, 62);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(3, 395);
+            this.panel2.TabIndex = 5;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(71, 169);
+            this.pictureBox1.Location = new System.Drawing.Point(95, 208);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(264, 83);
+            this.pictureBox1.Size = new System.Drawing.Size(352, 102);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
-            // panel1
+            // panel3
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.totalplayers);
-            this.panel1.Controls.Add(this.nplayers);
-            this.panel1.Controls.Add(this.btnDiscordServer);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.btnEntrar);
-            this.panel1.Location = new System.Drawing.Point(22, 33);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(818, 420);
-            this.panel1.TabIndex = 5;
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1151, 41);
+            this.panel3.TabIndex = 6;
+            this.panel3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseMove);
             // 
-            // panel2
+            // label5
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(69)))), ((int)(((byte)(121)))));
-            this.panel2.Location = new System.Drawing.Point(389, 50);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(2, 321);
-            this.panel2.TabIndex = 5;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(1015, 577);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Made by : Cabaret#3838";
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(863, 487);
+            this.ClientSize = new System.Drawing.Size(1151, 599);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(1151, 599);
+            this.MinimumSize = new System.Drawing.Size(1151, 599);
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Portugália Launcher";
             this.Load += new System.EventHandler(this.Main_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnEntrar;
-        private System.Windows.Forms.Label nplayers;
-        private System.Windows.Forms.Label totalplayers;
         private System.Windows.Forms.Button btnDiscordServer;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- FiveM Launcher/FiveM Launcher/Main.cs
+++ FiveM Launcher/FiveM Launcher/Main.cs
@@ -20,6 +20,9 @@ namespace FiveM_Launcher
 
         private String players_total; //Número de Jogadores no total
 
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
         (
@@ -28,17 +31,17 @@ namespace FiveM_Launcher
             int nRightRect,    // x-coordinate of lower-right corner
             int nBottomRect,   // y-coordinate of lower-right corner
             int nWidthEllipse, // height of ellipse
-            int nHeightEllipse // width of ellipse
+            int nHeightEllipse // width of ellipse  
         );
 
         private void Main_Load(object sender, EventArgs e)
         {
             try
             {
-                loadData();
                 panel1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel1.Width, panel1.Height, 20, 20));
                 btnDiscordServer.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btnDiscordServer.Width, btnDiscordServer.Height, 20, 20));
                 btnEntrar.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btnEntrar.Width, btnEntrar.Height, 20, 20));
+                loadData();
             }
             catch (Exception ex)
             {
@@ -53,10 +56,19 @@ namespace FiveM_Launcher
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start(fivemServer);
-            openNotification();
-            Thread.Sleep(3000);
-            Close();
+            try
+            {
+                System.Diagnostics.Process.Start(fivemServer);
+                openNotification();
+                Thread.Sleep(3000);
+                Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Não tens o FiveM instalado!");
+            }
+            
         }
 
         private void btnDiscordServer_Click(object sender, EventArgs e)
@@ -64,10 +76,24 @@ namespace FiveM_Launcher
             System.Diagnostics.Process.Start(discordServer);
         }
 
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void panel3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
         private void openNotification()
         {
             var title = "Portugália RP";
-            var message = "O FiveM está a ser aberto, aguarda! 😊";
+            var message = "O FiveM está a ser aberto, aguarda!";
 
             var iconUri = "file:///" + Path.GetFullPath("Images/logo-top.png");
             var imageUri = "file:///" + Path.GetFullPath("Images/logo-top.jpg");
@@ -103,8 +129,19 @@ namespace FiveM_Launcher
             players_ativo = Convert.ToString(servidor.Data.clients);
             players_total = Convert.ToString(servidor.Data.sv_maxclients);
 
-            nplayers.Text = players_ativo;
-            totalplayers.Text = players_total;
+            label3.Text = "Jogadores : " + players_ativo + " / " + players_total;
+
+            if((Convert.ToInt32(players_ativo) == 0) || (players_ativo == null))
+            {
+                label4.ForeColor = Color.Red;
+                label4.Text = "Offline";
+            } else
+            {
+                label4.ForeColor = Color.Green;
+                label4.Text = "Online";
+            }
         }
+
+   
     }
 }
