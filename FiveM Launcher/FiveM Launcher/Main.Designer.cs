ff3b8e47de92e945d5ddfd9a852baccc60ca64e0
JustCabaret <hugocabaretdasilva@gmail.com>
2022-06-01 16:09:35 +0100

    Mudança de Ícones Adição de funcionalidades.

--- FiveM Launcher/FiveM Launcher/Main.Designer.cs
+++ FiveM Launcher/FiveM Launcher/Main.Designer.cs
@@ -29,26 +29,32 @@ namespace FiveM_Launcher
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btnEntrar = new System.Windows.Forms.Button();
             this.nplayers = new System.Windows.Forms.Label();
             this.totalplayers = new System.Windows.Forms.Label();
             this.btnDiscordServer = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Location = new System.Drawing.Point(73, 91);
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Location = new System.Drawing.Point(494, 164);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(160, 88);
+            this.btnEntrar.Size = new System.Drawing.Size(219, 88);
             this.btnEntrar.TabIndex = 0;
-            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.Text = "Entrar no Servidor";
             this.btnEntrar.UseVisualStyleBackColor = true;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
             // 
             // nplayers
             // 
             this.nplayers.AutoSize = true;
-            this.nplayers.Location = new System.Drawing.Point(104, 41);
+            this.nplayers.Location = new System.Drawing.Point(541, 47);
             this.nplayers.Name = "nplayers";
             this.nplayers.Size = new System.Drawing.Size(35, 13);
             this.nplayers.TabIndex = 1;
@@ -57,7 +63,7 @@ namespace FiveM_Launcher
             // totalplayers
             // 
             this.totalplayers.AutoSize = true;
-            this.totalplayers.Location = new System.Drawing.Point(169, 40);
+            this.totalplayers.Location = new System.Drawing.Point(606, 46);
             this.totalplayers.Name = "totalplayers";
             this.totalplayers.Size = new System.Drawing.Size(35, 13);
             this.totalplayers.TabIndex = 2;
@@ -65,28 +71,57 @@ namespace FiveM_Launcher
             // 
             // btnDiscordServer
             // 
-            this.btnDiscordServer.Location = new System.Drawing.Point(73, 219);
+            this.btnDiscordServer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDiscordServer.Location = new System.Drawing.Point(494, 279);
             this.btnDiscordServer.Name = "btnDiscordServer";
-            this.btnDiscordServer.Size = new System.Drawing.Size(160, 88);
+            this.btnDiscordServer.Size = new System.Drawing.Size(219, 88);
             this.btnDiscordServer.TabIndex = 3;
-            this.btnDiscordServer.Text = "Entrar";
+            this.btnDiscordServer.Text = "Abrir o Discord";
             this.btnDiscordServer.UseVisualStyleBackColor = true;
             this.btnDiscordServer.Click += new System.EventHandler(this.btnDiscordServer_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(71, 169);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.totalplayers);
+            this.panel1.Controls.Add(this.nplayers);
+            this.panel1.Controls.Add(this.btnDiscordServer);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnEntrar);
+            this.panel1.Location = new System.Drawing.Point(22, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(818, 420);
+            this.panel1.TabIndex = 5;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnDiscordServer);
-            this.Controls.Add(this.totalplayers);
-            this.Controls.Add(this.nplayers);
-            this.Controls.Add(this.btnEntrar);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(863, 487);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -96,6 +131,8 @@ namespace FiveM_Launcher
         private System.Windows.Forms.Label nplayers;
         private System.Windows.Forms.Label totalplayers;
         private System.Windows.Forms.Button btnDiscordServer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- FiveM Launcher/FiveM Launcher/Main.cs
+++ FiveM Launcher/FiveM Launcher/Main.cs
@@ -1,31 +1,42 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FiveM_Launcher
 {
     public partial class Main : Form
     {
-        private static string fivemServer = "fivem://connect/cfx.re/join/kx5la6"; //Link do servidor FiveM
+        private static string fivemAPI = "https://servers-frontend.fivem.net/api/servers/single/b4opvp"; //API do servidor FiveM
+        private static string fivemServer = "fivem://connect/cfx.re/join/b4opvp"; //Link do servidor FiveM
+
         private static string discordServer = "https://discord.gg/GWZsjkJ"; //Link do servidor Discord
         private String players_ativo; //Número de Jogadores no momento
+
         private String players_total; //Número de Jogadores no total
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
 
         private void Main_Load(object sender, EventArgs e)
         {
             try
             {
                 loadData();
+                panel1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panel1.Width, panel1.Height, 20, 20));
             }
             catch (Exception ex)
             {
@@ -41,6 +52,7 @@ namespace FiveM_Launcher
         private void btnEntrar_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Process.Start(fivemServer);
+            openNotification();
             Thread.Sleep(3000);
             Close();
         }
@@ -50,11 +62,29 @@ namespace FiveM_Launcher
             System.Diagnostics.Process.Start(discordServer);
         }
 
+        private void openNotification()
+        {
+            var title = "Portugália RP";
+            var message = "O FiveM está a ser aberto, aguarda! 😊";
+
+            var iconUri = "file:///" + Path.GetFullPath("Images/logo-top.png");
+            var imageUri = "file:///" + Path.GetFullPath("Images/logo-top.jpg");
+
+            new ToastContentBuilder()
+            .AddArgument("action", "viewConversation")
+            .AddArgument("conversationId", 9813)
+            .AddAppLogoOverride(new Uri(iconUri), ToastGenericAppLogoCrop.Circle)
+            .AddHeroImage(new Uri(imageUri))
+            .AddText(title)
+            .AddText(message)
+
+            .Show();
+        }
+
         private void loadData()
         {
             var result = "";
-            var url = "https://servers-frontend.fivem.net/api/servers/single/kx5la6";
-            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            var httpRequest = (HttpWebRequest)WebRequest.Create(fivemAPI);
 
             httpRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36";
             httpRequest.Accept = "application/json";
